8199836146d076deaccd891cbd6d47317152ff38
UmeshDesaboina <149916679+UmeshDesaboina@users.noreply.github.com>
2025-10-26 13:40:14 +0530

    Delete frontend directory

--- frontend/src/components/ErrorMessage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ErrorMessage = ({ msg }) => (
-  <div style={{ color: 'red', textAlign: 'center', padding: '10px' }}>{msg}</div>
-);
-
-export default ErrorMessage;
--- frontend/src/components/QRCode.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import QRCodeReact from 'qrcode.react';
-
-const QRCode = ({ value }) => <QRCodeReact value={value} />;
-
-export default QRCode;
